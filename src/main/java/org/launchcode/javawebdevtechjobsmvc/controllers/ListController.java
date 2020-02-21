7afe2bb367d89c29f9e22d07a6189a06b1fb1515
Matt Alvarado <matimat2010@gmail.com>
2020-02-20 20:45:16 -0600

    Cleanup - Using Fragments

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
@@ -43,7 +43,6 @@ public class ListController {
         model.addAttribute("locations", JobData.getAllLocations());
         model.addAttribute("positions", JobData.getAllPositionTypes());
         model.addAttribute("skills", JobData.getAllCoreCompetency());
-        // model.addAttribute("all",JobData.findAll());
         return "list";
     }
 
